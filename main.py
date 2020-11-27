3e581acce743435a5a8d2210191ffd2e0a950c43
Gareth Tribello <gareth.tribello@gmail.com>
2020-11-27 12:24:32 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+def numberOfHundreds( N ) :
+  # Your code goes here
+  return 
+
+# This calls the function you have written to check if it works
+print( numberOfHundreds(3260) )
